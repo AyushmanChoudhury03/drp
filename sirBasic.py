e526e23ff7bb06130767ec7485650dabd457fe8e
Ayushman Choudhury <94494533+AyushmanChoudhury03@users.noreply.github.com>
2023-12-06 11:03:52 -0500

    Choose better numbers for plot clarity

--- sirBasic.py
+++ sirBasic.py
@@ -8,17 +8,17 @@ import matplotlib.pyplot as plt
 # Basic model variables
 
 N = 1000            # Total population
-S0 = 999            # Initial members of Susceptible, 
-I0 = 1              # ... infected
-R0 = N - S0 - I0    # ... recovered
+S0 = 990            # Initial members of Susceptible, 
+I0 = 10             # ... infected
+R0 = 0              # ... recovered
 y0 = S0, I0, R0     # Initial conditions vector
 
-beta = 1            # Beta: product of (contact rate) * (transmission probability)
-gamma = 0.1         # Gamma: mean recovery rate. 1/gamma is the average infectious period
-t = np.linspace(0, 50, 50) # A grid of time points (in days)
+beta = 50          # Beta: product of (contact rate) * (transmission probability)
+gamma = 5          # Gamma: mean recovery rate. 1/gamma is the average infectious period
+t = np.linspace(0, 365, 365) # A grid of time points (in days)
 
 # SIR differential equations.
-def diffeq(y, t, N, beta, gamma):
+def deriv(y, t, N, beta, gamma):
     S, I, R = y
     dSdt = -beta * (S / N) * (I / N)
     dIdt = beta * (S / N) * (I / N) - gamma * (I / N)
